d752c9f51db8e26e6f08035d406b457fc34a6583
wnstjd9701 <wnstjd9701@naver.com>
2023-04-13 10:15:45 +0900

    README.md

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ MAX_POST = 5
 
 # 기본적으로 바뀌지 않을 Markdown text 입력
 markdown_text = """
+<div class='blog' align='center'>
+  <h2> BLOG </h2>
+
+[![Tistory's Badge](https://github-readme-tistory-card.vercel.app/api/badge?name=준성`s블로그&theme=kakao)](https://dev-wnstjd.tistory.com)
+
+</div>
+<hr>
+<div class='tech-stack' align='center'>
+  <h2> 📚 TECH STACK 🛠 </h2>
+  <span stye="">
+  <img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white">&nbsp
+  <img src="https://img.shields.io/badge/node.js-339933?style=for-the-badge&logo=Node.js&logoColor=white">&nbsp
+  <img src="https://img.shields.io/badge/mysql-4479A1?style=for-the-badge&logo=mysql&logoColor=white">&nbsp 
+  <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">&nbsp
+  <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">&nbsp
+  <img src="https://img.shields.io/badge/amazonaws-232F3E?style=for-the-badge&logo=amazonaws&logoColor=white">&nbsp
+  </span>
+</div>
 ## ✅ Latest Tistory Posting
 """
 
