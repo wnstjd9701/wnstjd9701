d69c549d360a8ce1d06a1303c557b9bbf50cb67d
wnstjd9701 <wnstjd9701@naver.com>
2023-04-13 10:12:55 +0900

    Add: Lastest Blog Post

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ URL="https://dev-wnstjd.tistory.com/rss" # URL = "내블로그 주소/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
+# 기본적으로 바뀌지 않을 Markdown text 입력
 markdown_text = """
 ## ✅ Latest Tistory Posting
 """
@@ -13,8 +14,8 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         break
     else:
         feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-        print(markdown_text)
+        markdown_text += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']})\n"
+
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
